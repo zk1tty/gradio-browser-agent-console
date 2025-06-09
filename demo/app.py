a6e7f6a40be6b70b0dd102aa9a0150d8dcb09885
zk1tty <norikazkitty@gmail.com>
2025-06-09 13:37:34 +0800

    add interactive logging(handle) in Gradio app with state management

--- demo/app.py
+++ demo/app.py
@@ -2,16 +2,21 @@
 import gradio as gr
 from gradio_gradio_browser_agent_console import gradio_browser_agent_console
 
+def handle(action: str, state: str):
+    state += f"> {action}\n"
+    return state, state         # update both copies
 
-example = gradio_browser_agent_console().example_value()
+with gr.Blocks() as demo:
+    log_state = gr.State("") 
+    console = gradio_browser_agent_console()
 
-demo = gr.Interface(
-    lambda x:x,
-    gradio_browser_agent_console(),  # interactive version of your component
-    gradio_browser_agent_console(),  # static version of your component
-    # examples=[[example]],  # uncomment this line to view the "example version" of your component
-)
-
+    # wire the change event yourself
+    console.change(
+        fn=handle,
+        inputs=[console, log_state],
+        outputs=[console], 
+        queue=False               # keep it snappy
+    )
 
 if __name__ == "__main__":
     demo.launch()
